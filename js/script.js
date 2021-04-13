fd87d03828930950351f9bdad18c59a694166785
Billy King <billyjack30@gmail.com>
2021-04-13 13:19:03 -0400

    added some functions and button ids

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
+function addEnteredNumbers(a, b) {
+    return a + b;
+}
+
+function add() {
+    let firstNumber = parseFloat(newTest)
+    console.log(firstNumber);
+}
+
 const display = document.getElementById('display');
+const addButton = document.getElementById('add');
+const subtractButton = document.getElementById('subtract');
+const multiplyButton = document.getElementById('multiply');
+const divideButton = document.getElementById('divide');
+const equalsButton = document.getElementById('equals')
 
-const button = document.querySelectorAll('.calc-buttons button');
+const numButton = document.querySelectorAll('.calc-buttons .number');
 
-button.forEach(item => {
+let newTest = 0;
+
+numButton.forEach(item => {
     item.addEventListener('click', event => {
-        console.log(item.textContent);
+        if (newTest === undefined || newTest === 0) {
+            newTest = item.textContent;
+            console.log(item.textContent);
+        } else {
+            newTest += item.textContent;
+            console.log(item.textContent);
+        }
+        display.textContent = newTest;
     })
 })
+
+addButton.addEventListener('click', event => add());
